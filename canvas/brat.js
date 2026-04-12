7b86c920c50c10b960ff29a9dd00124c30129d00
teixeiraweverton399-wq <teixeiraweverton399@gmail.com>
2026-04-12 00:18:19 -0300

    Update Vex API key and query text in brat.js

--- canvas/brat.js
+++ canvas/brat.js
@@ -51,8 +51,8 @@ function baixarImagem(url, destino) {
 // ==========================
 // CONFIGURAÇÃO
 // ==========================
-const apikey = '5c9c1d4b-900e-4892-8f2c-24e31a51a614'; // Sua chave Vex API
-const query = 'Vex API'; // Texto que vai aparecer na imagem
+const apikey = '81d291be-9693-44e4-b405-2a23a5b6b4b2'; // Sua chave Vex API
+const query = 'brat'; // Texto que vai aparecer na imagem
 
 // URL completa da API Brat
 const urlAPI = `https://vexapi.com.br/api/canvas/brat?apikey=${apikey}&query=${encodeURIComponent(query)}`;
